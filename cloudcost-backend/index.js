f55b390472fd98a76e556519b2e710be6167a169
tanishaaraoo07 <damalatanisha7@gmail.com>
2025-08-02 01:25:58 +0530

    Add cookie-parser dependency

--- cloudcost-backend/index.js
+++ cloudcost-backend/index.js
@@ -18,8 +18,11 @@ app.use(cors({
 }));
 
 // MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ Mongo error:", err));
 
