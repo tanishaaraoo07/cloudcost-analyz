b157fdb0c06c79a5240934209c1b6d3ecbc0e893
tanishaaraoo07 <damalatanisha7@gmail.com>
2025-08-02 00:23:52 +0530

    fixed the uri/ index.js

--- cloudcost-frontend/src/api.js
+++ cloudcost-frontend/src/api.js
@@ -1,8 +1,11 @@
-axios.post("https://cloudcost-analyz.onrender.com/api/auth/login", {
-  email,
-  password
-}, {
-  withCredentials: true
-});
+fetch("https://cloudcost-analyz.onrender.com/api/auth/login", {
+  method: "POST",
+  credentials: "include", // only if you use cookies/session
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ email, password })
+})
+
 
 
