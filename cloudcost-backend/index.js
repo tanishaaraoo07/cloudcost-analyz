9017ee92b82a64eabc13c34505d57f7d456ce5db
tanishaaraoo07 <damalatanisha7@gmail.com>
2025-08-17 20:26:34 +0530

    fix index.js

--- cloudcost-backend/index.js
+++ cloudcost-backend/index.js
@@ -48,7 +48,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 // ✅ 5. API Routes
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/cloud", require("./routes/cloud"));
+app.use("/", require("./routes/cloud"));
 
 // ✅ 6. Health Check
 app.get("/", (req, res) => {
